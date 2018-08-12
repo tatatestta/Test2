9a30e2ba9f55ff55553ae124bb143b086dc8e537
Test <tatatestta@gmail.com>
2018-08-12 13:44:16 +0300

    change

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world !!!!!");
+        int x = 5;
+        int[] array = {1, 2, 4, 4};
     }
 }
